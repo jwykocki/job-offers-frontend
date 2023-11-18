75c513bbd5ee6e98a1f58e056fc02deb712b8f5c
Kuba <jakub.wykocki@wp.pl>
2023-11-18 12:06:13 +0100

    connected to simple json server

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
 
   
   return (
+    <Router>
     <div className="App">
-      <Home />
+      <div className='content'>
+        <Switch>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+        </div>
       
     </div>
+    </Router>
   );
 }
 
